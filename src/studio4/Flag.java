1358091e988dc34b2e3ef6c1a41435cfae3d689e
kevinchen <kevinchen@localhost>
2022-10-12 16:40:47 -0500

    Studio 4 flag

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -5,7 +5,18 @@ import java.awt.Color;
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
+		StdDraw.setXscale(0,10);
+		StdDraw.setYscale(0,10);
+		StdDraw.setPenColor(255, 0, 0);
 		
+		StdDraw.filledRectangle(1,1,1,3);
+		StdDraw.setPenColor(0,0,255);
+		StdDraw.filledRectangle(3, 1,1 ,3);
+		StdDraw.setPenColor(0,0,0);
+		
+		StdDraw.filledRectangle(5, 1, 1, 3);
+	
 	}
 }
